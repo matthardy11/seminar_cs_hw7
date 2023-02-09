e1678f96b0b8ffa03809b11c8a5e4aafad4c84aa
matthardy11 <matthardy2560@gmail.com>
2023-02-09 20:31:10 +0400

    complete task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+int InputNumber(string str)
+{
+  int number;
+  string text;
+  while(true)
+  {
+    Console.WriteLine(str);
+    text = Console.ReadLine();
+    if(int.TryParse(text, out number))
+    {
+      break;
+    }
+    System.Console.WriteLine("Введено не корректное число, попробуйте ещё раз.");
+  }
+  return number;
+}
+
+void FillArrayMaxtrixDouble(double[,] array)
+{
+  Random rand = new Random();
+  for(int i = 0; i < array.GetLength(0); i++)
+  {
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i,j] = Math.Round((rand.NextDouble() * rand.Next(0, 100)), 3);
+    }
+  }
+}
+
+void ShowArray(double[,] array)
+{
+  for(int i = 0; i < array.GetLength(0); i++)
+  {
+    for(int j = 0; j < array.GetLength(1); j++)
+    {
+      System.Console.Write(array[i,j] + "\t");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+int m = InputNumber("Введите m: ");
+int n = InputNumber("введите n: ");
+
+double[,] matrix = new double[m, n];
+
+FillArrayMaxtrixDouble(matrix);
+ShowArray(matrix);
